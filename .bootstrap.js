df2738f39333cb9e26a670d8744558601100c861
Vibesdk <vibesdk-bot@cloudflare.com>
2026-04-02 15:19:11 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -78,7 +78,8 @@ function updateWranglerJsonc() {
 
 function runSetupCommands() {
     const commands = [
-    "bun add react-dropzone@^14.2.3"
+    "bun add react-dropzone@^14.2.3",
+    "bun add @hookform/resolvers zod"
 ];
     
     if (commands.length === 0) {
